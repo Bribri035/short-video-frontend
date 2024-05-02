91d3b4164ff29a7aa65376c7f690c4c9cc8520bf
Brian Turner <brianturnercs@gmail.com>
2024-05-01 21:13:24 -0600

    updated for heroku backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:"http://localhost:9000"
+    baseURL:"https://short-video-app-turner-7348e2e9219e.herokuapp.com/"
 })
 
 export default instance
